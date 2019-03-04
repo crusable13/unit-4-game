2d203cfb820f716c631106da40dcf862ee45c40f
Christopher <jcelitesmith@gmail.com>
2019-03-03 22:07:02 -0800

    Game working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,79 @@
 $(document).ready(function(){
    
-    let crystalOne;
-    let crystalTwo;
-    let crystalThree;
-    let crystalFour;
-    let total = 0;
-    let target = 0;
-    let wins = 0;
-    let losses = 0;
+  let crystalOne;
+  let crystalTwo;
+  let crystalThree;
+  let crystalFour;
+  let total = 0;
+  let target = 0;
+  let wins = 0;
+  let losses = 0;
 
 
-    function setTarget() {
-      let upperLimitScore = 120;
-      let lowerLimitScore = 19;
-      target = Math.floor(Math.random() * (upperLimitScore-lowerLimitScore)) + lowerLimitScore;
-      $("#target").html("Target: " + target);
-      // console.log(target);
-    }
-
-    function updateTotal () {
-       $(".btn").on("click",function(event){
-         console.log(event);
-        // console.log("you just clicked " + );
-        // console.log(this.getAttribute("data-id"));
-        // console.log($(this).attr("data-id"));
-      });
-    }
-    
-    
-    
-    function crystalRandom() {
-      let crystalCeiling = 12;
-      let crystalFloor = 2;
-      let crystalValue = Math.floor(Math.random() * (crystalCeiling-crystalFloor)) + crystalFloor;
-      return randomValue;
-    } 
-
-    
-   
-
-    function initializeReset (){
-      crystalOne = crystalRandom();
-      crystalTwo = crystalRandom();
-      crystalThree = crystalRandom();
-      crystalFour = crystalRandom();
-      setTarget();
-      $("#total").html("Total: " + total);
-      $("#wins").html("Wins: " + wins);
-      $("#losses").html("Losses: " + losses);
-    }
-
-  function winLose() {
-    
+  function setTarget() {
+    let upperLimitScore = 120;
+    let lowerLimitScore = 19;
+    target = Math.floor(Math.random() * (upperLimitScore-lowerLimitScore)) + lowerLimitScore;
+    $("#target").html("Target: " + target);
+    // console.log(target);
   }
 
-    initializeReset(); 
-    
+  function updateTotal (){
+    total += $(this).data("data-value");
+    $("#total").html("Total: " + total);
+    if (total >= target) {
+      winLose();
+    }
+
+  }
+
+  function crystalRandom() {
+    let crystalCeiling = 12;
+    let crystalFloor = 2;
+    let crystalValue = Math.floor(Math.random() * (crystalCeiling-crystalFloor)) + crystalFloor;
+    return crystalValue;
+  }  
+
+  function initializeReset (){
+    crystalOne = $("[data-id=1]");
+    crystalOne.data("data-value", crystalRandom()); 
+    console.log("Crystal 1: " + crystalOne.data("data-value"));
+    crystalTwo = $("[data-id=2]");
+    crystalTwo.data("data-value", crystalRandom()); 
+    console.log("Crystal 2: " + crystalTwo.data("data-value"));
+    crystalThree = $("[data-id=3]");
+    crystalThree.data("data-value", crystalRandom()); 
+    console.log("Crystal 3: " + crystalThree.data("data-value"));
+    crystalFour = $("[data-id=4]");
+    crystalFour.data("data-value", crystalRandom()); 
+    console.log("Crystal 4: " + crystalFour.data("data-value"));
+    setTarget();
+    total = 0;
+    console.log("Target: " + target);
+    $("#total").html("Total: " + total);
+    $("#wins").html("Wins: " + wins);
+    $("#losses").html("Losses: " + losses);
+  }
+
+function winLose() {
+  if (total === target) {
+    wins++;
+    $("#wins").html("Wins: " + wins);
+    console.log("You win!");
+  } else {
+    losses++;
+    $("#losses").html("Losses: " + losses);
+    console.log("You lose!");
+  }
+  initializeReset(); 
+}
+
+$(".btn").on("click", updateTotal);
+
+
+
+  initializeReset(); 
+  
 
 
 })
